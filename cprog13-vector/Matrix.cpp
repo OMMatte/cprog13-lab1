47beba1740055fe24f87f10e484ff8dccee0e31d
Lucas Wiener <lucas@wiener.se>
2014-01-10 16:33:20 +0100

    Reformated Matrix.h

--- cprog13-vector/Matrix.cpp
+++ cprog13-vector/Matrix.cpp
@@ -1,14 +1,9 @@
 //
-//  Matrix.h
+//  Matrix.cpp
 //  cprog13-vector
 //
 //  Created by Lucas Wiener on 1/10/14.
 //  Copyright (c) 2014 cprog. All rights reserved.
 //
 
-#ifndef cprog13_vector_Matrix_h
-#define cprog13_vector_Matrix_h
-
-
-
-#endif
+#include "Matrix.h"
